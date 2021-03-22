ddb7deab9520ce418a219c428c64bf5e68b15a4a
Andreas Stassivik <andreas@stassi.net>
2021-03-22 12:54:21 -0700

    type declaration and semantic name

--- src/chunks.test.ts
+++ src/chunks.test.ts
@@ -1,23 +1,24 @@
 import chunks from './chunks'
 
-const testName = (n: number) => `Return the correct length of ${n} chunk(s).`
+const chunkTestName = (n: number): string =>
+  `Return the correct length of ${n} chunk(s) in meters.`
 
-test(testName(-1), () => {
+test(chunkTestName(-1), () => {
   expect(chunks(-1)).toBe(-16)
 })
 
-test(testName(0), () => {
+test(chunkTestName(0), () => {
   expect(chunks(0)).toBe(0)
 })
 
-test(testName(0.5), () => {
+test(chunkTestName(0.5), () => {
   expect(chunks(0.5)).toBe(8)
 })
 
-test(testName(1), () => {
+test(chunkTestName(1), () => {
   expect(chunks(1)).toBe(16)
 })
 
-test(testName(2), () => {
+test(chunkTestName(2), () => {
   expect(chunks(2)).toBe(32)
 })
