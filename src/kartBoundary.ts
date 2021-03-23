dc743062df7a14f1feb89263d028f10143b361da
Andreas Stassivik <andreas@stassi.net>
2021-03-22 17:23:03 -0700

    implement kartLength

--- src/kartBoundary.ts
+++ src/kartBoundary.ts
@@ -1,3 +1,5 @@
+import kartLength from './kartLength'
+
 export default function kartBoundary(zoom: number): (x: number) => number {
-  return (x: number): number => 2 ** (zoom + 7) * (x - 0.5)
+  return (x: number): number => kartLength(zoom) * (x - 0.5)
 }
