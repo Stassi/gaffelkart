5a840e9206473c9d0fdcd7e6a87f714d4f31b235
Andreas Stassivik <andreas@stassi.net>
2021-03-22 17:09:02 -0700

    extract test data

--- src/chunks.test.ts
+++ src/chunks.test.ts
@@ -2,22 +2,30 @@ import chunks from './chunks'
 
 const chunkTestName = (n: number): string => `length of ${n} chunk(s) in meters`
 
+const expected = {
+  '-1': -16,
+  0: 0,
+  0.5: 8,
+  1: 16,
+  2: 32,
+}
+
 test(chunkTestName(-1), () => {
-  expect(chunks(-1)).toBe(-16)
+  expect(chunks(-1)).toBe(expected[-1])
 })
 
 test(chunkTestName(0), () => {
-  expect(chunks(0)).toBe(0)
+  expect(chunks(0)).toBe(expected[0])
 })
 
 test(chunkTestName(0.5), () => {
-  expect(chunks(0.5)).toBe(8)
+  expect(chunks(0.5)).toBe(expected[0.5])
 })
 
 test(chunkTestName(1), () => {
-  expect(chunks(1)).toBe(16)
+  expect(chunks(1)).toBe(expected[1])
 })
 
 test(chunkTestName(2), () => {
-  expect(chunks(2)).toBe(32)
+  expect(chunks(2)).toBe(expected[2])
 })
