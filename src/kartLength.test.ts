fbc92148957142bb739ff9a14009c533cd43497b
Andreas Stassivik <andreas@stassi.net>
2021-03-22 17:30:51 -0700

    template literal extraction

--- src/kartLength.test.ts
+++ src/kartLength.test.ts
@@ -1,23 +1,26 @@
 import kartLength from './kartLength'
 
+const kartLengthTestName = (zoom: number): string =>
+  `length of zoom-${zoom} map in meters`
+
 const expected = { 0: 128, 1: 256, 2: 512, 3: 1024, 4: 2048 }
 
-test('length of zoom-0 map', () => {
+test(kartLengthTestName(0), () => {
   expect(kartLength(0)).toBe(expected[0])
 })
 
-test('length of zoom-1 map', () => {
+test(kartLengthTestName(1), () => {
   expect(kartLength(1)).toBe(expected[1])
 })
 
-test('length of zoom-2 map', () => {
+test(kartLengthTestName(2), () => {
   expect(kartLength(2)).toBe(expected[2])
 })
 
-test('length of zoom-3 map', () => {
+test(kartLengthTestName(3), () => {
   expect(kartLength(3)).toBe(expected[3])
 })
 
-test('length of zoom-4 map', () => {
+test(kartLengthTestName(4), () => {
   expect(kartLength(4)).toBe(expected[4])
 })
