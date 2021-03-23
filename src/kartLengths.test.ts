64acd57fc4855a9137441687a7ce5ea24ba2c69e
Andreas Stassivik <andreas@stassi.net>
2021-03-22 22:47:07 -0700

    test segmentation

--- src/kartLengths.test.ts
+++ src/kartLengths.test.ts
@@ -1,6 +1,6 @@
 import kartLengths from './kartLengths'
 
-describe('#kartLengths', () => {
+describe('map lengths', () => {
   const kartLengthTestName = (zoom: number): string =>
     `length of zoom-${zoom} map in meters`
 
@@ -27,34 +27,34 @@ describe('#kartLengths', () => {
   test(kartLengthTestName(4), () => {
     expect(lengthOne(4)).toBe(expectedLength[4])
   })
+})
 
-  describe.each([
-    [0, { '-1': -192, 0: -64, 0.5: 0, 1: 64, 2: 192 }],
-    [1, { '-1': -384, 0: -128, 0.5: 0, 1: 128, 2: 384 }],
-    [2, { '-1': -768, 0: -256, 0.5: 0, 1: 256, 2: 768 }],
-    [3, { '-1': -1536, 0: -512, 0.5: 0, 1: 512, 2: 1536 }],
-    [4, { '-1': -3072, 0: -1024, 0.5: 0, 1: 1024, 2: 3072 }],
-  ])('zoom-%i boundary', (zoom, expected) => {
-    const position = kartLengths({ zoom, boundary: true })
+describe.each([
+  [0, { '-1': -192, 0: -64, 0.5: 0, 1: 64, 2: 192 }],
+  [1, { '-1': -384, 0: -128, 0.5: 0, 1: 128, 2: 384 }],
+  [2, { '-1': -768, 0: -256, 0.5: 0, 1: 256, 2: 768 }],
+  [3, { '-1': -1536, 0: -512, 0.5: 0, 1: 512, 2: 1536 }],
+  [4, { '-1': -3072, 0: -1024, 0.5: 0, 1: 1024, 2: 3072 }],
+])('zoom-%i boundary', (zoom, expected) => {
+  const position = kartLengths({ zoom, boundary: true })
 
-    test('position at -1', () => {
-      expect(position(-1)).toBe(expected[-1])
-    })
+  test('position at -1', () => {
+    expect(position(-1)).toBe(expected[-1])
+  })
 
-    test('position at 0', () => {
-      expect(position(0)).toBe(expected[0])
-    })
+  test('position at 0', () => {
+    expect(position(0)).toBe(expected[0])
+  })
 
-    test('position at 0.5', () => {
-      expect(position(0.5)).toBe(expected[0.5])
-    })
+  test('position at 0.5', () => {
+    expect(position(0.5)).toBe(expected[0.5])
+  })
 
-    test('position at 1', () => {
-      expect(position(1)).toBe(expected[1])
-    })
+  test('position at 1', () => {
+    expect(position(1)).toBe(expected[1])
+  })
 
-    test('position at 2', () => {
-      expect(position(2)).toBe(expected[2])
-    })
+  test('position at 2', () => {
+    expect(position(2)).toBe(expected[2])
   })
 })
