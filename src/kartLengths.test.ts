a7476c2ac0c12d590655b955907653b2525bdb09
Andreas Stassivik <andreas@stassi.net>
2021-03-22 22:36:25 -0700

    disambiguation

--- src/kartLengths.test.ts
+++ src/kartLengths.test.ts
@@ -4,28 +4,28 @@ describe('#kartLengths', () => {
   const kartLengthTestName = (zoom: number): string =>
     `length of zoom-${zoom} map in meters`
 
-  const expected = { 0: 128, 1: 256, 2: 512, 3: 1024, 4: 2048 }
+  const expectedLength = { 0: 128, 1: 256, 2: 512, 3: 1024, 4: 2048 }
 
   const lengthOne = (zoom: number): number => kartLengths({ zoom })(1)
 
   test(kartLengthTestName(0), () => {
-    expect(lengthOne(0)).toBe(expected[0])
+    expect(lengthOne(0)).toBe(expectedLength[0])
   })
 
   test(kartLengthTestName(1), () => {
-    expect(lengthOne(1)).toBe(expected[1])
+    expect(lengthOne(1)).toBe(expectedLength[1])
   })
 
   test(kartLengthTestName(2), () => {
-    expect(lengthOne(2)).toBe(expected[2])
+    expect(lengthOne(2)).toBe(expectedLength[2])
   })
 
   test(kartLengthTestName(3), () => {
-    expect(lengthOne(3)).toBe(expected[3])
+    expect(lengthOne(3)).toBe(expectedLength[3])
   })
 
   test(kartLengthTestName(4), () => {
-    expect(lengthOne(4)).toBe(expected[4])
+    expect(lengthOne(4)).toBe(expectedLength[4])
   })
 
   describe.each([
